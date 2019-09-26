607356d1b6a6f5d848ec17bb2870850690125e82
Mike Eheler <mike.eheler@gmail.com>
2019-09-25 22:32:40 -0700

    Initial Vulkan

--- include/hello_triangle_app.h
+++ include/hello_triangle_app.h
@@ -1,19 +1,26 @@
 #pragma once
 
 #include "scoped_glfw_window.h"
+#include <GLFW/glfw3.h>
+#include <memory>
 
 namespace vulkan_tutorial {
     class hello_triangle_app {
     public:
+        hello_triangle_app();
+        ~hello_triangle_app();
+
         void run();
 
     private:
         const int INITIAL_HEIGHT = 600;
         const int INITIAL_WIDTH = 800;
 
+        std::unique_ptr<VkInstance> _instance;
         scoped_glfw_window _window;
 
         void cleanup();
+        void createInstance();
         void initVulkan();
         void initWindow();
         void mainLoop();
--- src/hello_triangle_app.cpp
+++ src/hello_triangle_app.cpp
@@ -1,10 +1,46 @@
 #include "hello_triangle_app.h"
 #include "scoped_glfw_window.h"
 
+#include <cstdint>
+#include <iostream>
 #include <stdexcept>
+#include <vector>
 #include <GLFW/glfw3.h>
 
+namespace {
+    VkApplicationInfo create_vk_application_info() {
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        return appInfo;
+    }
+
+    void print_extensions() {
+        uint32_t extensionCount = 0u;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:" << std::endl;
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << std::endl;
+        }
+    }
+}
+
 namespace vulkan_tutorial {
+    hello_triangle_app::hello_triangle_app()
+        : _instance{new VkInstance()}
+    {}
+
+    hello_triangle_app::~hello_triangle_app() {
+        cleanup();
+    }
+
     void hello_triangle_app::run() {
         initWindow();
         initVulkan();
@@ -12,7 +48,40 @@ namespace vulkan_tutorial {
         cleanup();
     }
 
+    void hello_triangle_app::cleanup() {
+        if (_instance == nullptr)
+            return;
+
+        vkDestroyInstance(*_instance.get(), nullptr);
+        _instance.reset();
+        _window.destroy();
+        glfwTerminate();
+    }
+
+    void hello_triangle_app::createInstance() {
+        auto appInfo = create_vk_application_info();
+
+        uint32_t glfwExtensionCount = 0u;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        print_extensions();
+
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, _instance.get());
+        if (result != VK_SUCCESS)
+            throw std::runtime_error("Failed to create VkInstance");
+    }
+
     void hello_triangle_app::initVulkan() {
+        createInstance();
     }
 
     void hello_triangle_app::initWindow() {
@@ -30,10 +99,5 @@ namespace vulkan_tutorial {
         while (glfwWindowShouldClose(_window.get()) == GLFW_FALSE) {
             glfwPollEvents();
         }
-    }
-
-    void hello_triangle_app::cleanup() {
-        _window.destroy();
-        glfwTerminate();
     }
 }
